29ae963aef8a7a9e037bd862206dfd7c8b1470e3
Daimar <alex321solano@gmail.com>
2023-08-02 16:21:06 -0500

    Con el metodo addEventlistener se realizara un evento que al darle click en el icono de hamburguer mostrara el cambio llamando con su clase usando toggle que permite cada vez que se ejecute cambiar de estado la visibilidad del elemento HTML

---
+++ Main.js
@@ -0,0 +1,17 @@
+/*1 variable: Crear un evento con el cambio de las imagenes "Hamburguesa" y  "X" Es decir,*/ 
+const Icono = document.querySelector('.nav__hamburger');
+
+/*2 variable:  */
+const cerrar = document.querySelector('.nav__overlay');
+
+/*Esta variable es funcion de los elementos en desktop */
+const desaparecer = cerrar; //para esconder los menu.
+
+/*Guardar.
+Cada vez que le de click cambia el icono*/
+Icono.addEventListener('click', ()=>{
+    Icono.classList.toggle('nav__hamburguer--open');
+    /*Desde la Inspeccion estamos quitando y agg icono diferentes */
+    cerrar.classList.toggle('nav__overlay--show');
+    /*Lo que hará que se desaparezca y aparezca al darle click por el  evento "ICONO"*/
+}); 
